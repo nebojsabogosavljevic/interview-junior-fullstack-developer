cec44881bdc094e71c9adc0a46f0ada3af43b0a2
Nebojša <nebojsabogosvljevic3@gmail.com>
2023-07-28 18:45:56 +0200

    Added tests cases for service and controller

--- interview-backend/src/cities/cities.controller.spec.ts
+++ interview-backend/src/cities/cities.controller.spec.ts
@@ -1,20 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpException } from '@nestjs/common';
 import { CitiesController } from './cities.controller';
 import { CitiesService } from './cities.service';
 
 describe('CitiesController', () => {
-  let controller: CitiesController;
+  let citiesController: CitiesController;
+  let citiesService: CitiesService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [CitiesController],
       providers: [CitiesService],
     }).compile();
 
-    controller = module.get<CitiesController>(CitiesController);
+    citiesController = moduleRef.get<CitiesController>(CitiesController);
+    citiesService = moduleRef.get<CitiesService>(CitiesService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(citiesController).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return all cities from the service', () => {
+      const mockCities = [
+        { uuid: "7e8a29e2-62d1-4ec1-ae15-8ff2f777318f", cityName: 'Berlin', count: 523, imgCode: "de-be_c" }, 
+        { uuid: "49c01e96-2a36-47bc-862f-803de4e8bdae", cityName: 'Dresden', count: 198, imgCode: "de-sn-dd" }
+      ];
+      jest.spyOn(citiesService, 'findAll').mockReturnValue(mockCities);
+
+      expect(citiesController.findAll()).toEqual(mockCities);
+    });
+  });
+
+  describe('findByUUID', () => {
+    it('should throw a HttpException when UUID is not provided', () => {
+      expect(() => citiesController.findByUUID(undefined)).toThrow(HttpException);
+    });
+
+    it('should throw a HttpException when UUID length is not 36 characters', () => {
+      const invalidUUID = '7e8a29e2-62d1-4ec1-ae15-';
+      expect(() => citiesController.findByUUID(invalidUUID)).toThrow(HttpException);
+    });
+
+    it('should return the city with the specified UUID from the service', () => {
+      const mockUUID = '7e8a29e2-62d1-4ec1-ae15-8ff2f777318f';
+      const mockCity = { uuid: "7e8a29e2-62d1-4ec1-ae15-8ff2f777318f", cityName: 'Berlin', count: 523, imgCode: "de-be_c" };
+      jest.spyOn(citiesService, 'findByUUID').mockReturnValue(mockCity);
+
+      expect(citiesController.findByUUID(mockUUID)).toEqual(mockCity);
+    });
+
+    it('should throw a HttpException when no city is found with the given UUID', () => {
+      const nonExistentUUID = undefined;
+      jest.spyOn(citiesService, 'findByUUID').mockReturnValue(undefined);
+
+      expect(() => citiesController.findByUUID(nonExistentUUID)).toThrow(HttpException);
+    });
+  });
+
+  describe('findByCityName', () => {
+    it('should throw a HttpException when city name is not provided', () => {
+      expect(() => citiesController.findByCityName(undefined)).toThrow(HttpException);
+    });
+
+    it('should throw a HttpException when city name length is less than 3 characters', () => {
+      const shortCityName = 'AB';
+      expect(() => citiesController.findByCityName(shortCityName)).toThrow(HttpException);
+    });
+
+    it('should return cities with the specified city name from the service', () => {
+      const mockCityName = 'Berlin';
+      const mockCities = [
+        { uuid: "7e8a29e2-62d1-4ec1-ae15-8ff2f777318f", cityName: 'Berlin', count: 523, imgCode: "de-be_c" }, 
+        { uuid: "49c01e96-2a36-47bc-862f-803de4e8bdae", cityName: 'Dresden', count: 198, imgCode: "de-sn-dd" }
+      ];
+      jest.spyOn(citiesService, 'findByCityName').mockReturnValue(mockCities);
+
+      expect(citiesController.findByCityName(mockCityName)).toEqual(mockCities);
+    });
+
+    it('should throw a HttpException when no cities are found with the given city name', () => {
+      const nonExistentCityName = 'NonExistentCity';
+      jest.spyOn(citiesService, 'findByCityName').mockReturnValue([]);
+
+      expect(() => citiesController.findByCityName(nonExistentCityName)).toThrow(HttpException);
+    });
   });
 });
--- interview-backend/src/cities/cities.service.spec.ts
+++ interview-backend/src/cities/cities.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CitiesService } from './cities.service';
+import * as CITIES from '../../../cities.json';
 
 describe('CitiesService', () => {
   let service: CitiesService;
@@ -14,5 +15,35 @@ describe('CitiesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return all cities', () => {
+    const cities = service.findAll();
+    expect(cities).toEqual(CITIES);
+  });
+
+  it('should return a city by ID', () => {
+    const cityId = 1; // Replace this with a valid ID from your data
+    const city = service.findOne(cityId);
+    expect(city).toBe(`This action returns a #${cityId} city`);
+  });
+
+  it('should return a city by UUID', () => {
+    const cityUUID = '4a7f5c2d-3a10-4a02-a9b3-450839929e43'; 
+    const city = service.findByUUID(cityUUID);
+    expect(city).toEqual(expect.objectContaining({ uuid: cityUUID }));
+  });
+
+  it('should return cities by city name (case-insensitive)', () => {
+    const cityName = 'Berlin';
+    const matchedCities = service.findByCityName(cityName);
+    expect(matchedCities).toHaveLength(1);
+    expect(matchedCities[0].cityName.toLowerCase()).toContain(cityName.toLowerCase());
+  });
+
+  it('should return an empty array if no city name is matched', () => {
+    const cityName = 'NonExistentCityName';
+    const matchedCities = service.findByCityName(cityName);
+    expect(matchedCities).toEqual([]);
   });
 });
